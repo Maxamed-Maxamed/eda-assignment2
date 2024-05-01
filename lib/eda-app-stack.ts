c2b7d089cb86002acc54b5bcddc7f59d69fe2b75
Maxamed Maxamed <20088349@mail.wit.ie>
2024-05-01 18:25:14 +0100

    Completed Fan out implementation - added emailing logic

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -57,6 +57,45 @@ export class EDAAppStack extends cdk.Stack {
     maxBatchingWindow: cdk.Duration.seconds(10),
   });
 
+
+
+  const mailerQ = new sqs.Queue(this, "mailer-queue", {
+    receiveMessageWaitTime: cdk.Duration.seconds(10),
+  });
+
+  const mailerFn = new lambdanode.NodejsFunction(this, "mailer-function", {
+    runtime: lambda.Runtime.NODEJS_16_X,
+    memorySize: 1024,
+    timeout: cdk.Duration.seconds(3),
+    entry: `${__dirname}/../lambdas/mailer.ts`,
+  });
+
+
+
+  newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
+
+
+  const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
+    batchSize: 5,
+    maxBatchingWindow: cdk.Duration.seconds(10),
+  }); 
+
+
+  mailerFn.addToRolePolicy(
+    new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        "ses:SendEmail",
+        "ses:SendRawEmail",
+        "ses:SendTemplatedEmail",
+      ],
+      resources: ["*"],
+    })
+  );
+
+
+
+
   processImageFn.addEventSource(newImageEventSource);
 
   // Permissions
